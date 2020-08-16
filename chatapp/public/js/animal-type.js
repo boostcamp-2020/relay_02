32c65f0e3987c9b2bd135589d31d3cf9539b12c4
ashtapor25 <junsushin4546@gmail.com>
2020-08-16 15:12:18 +0900

    animal type is shown in console after image upload

---
+++ chatapp/public/js/animal-type.js
@@ -0,0 +1,48 @@
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+
+// the link to your model provided by Teachable Machine export panel
+const URL = "https://teachablemachine.withgoogle.com/models/mCPUgT_SH/";
+
+let model, animalTypeContainer, labelContainer, maxPredictions;
+
+// Load the image model and setup the webcam
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    labelContainer = document.getElementById("label-container");
+    animalTypeContainer = document.getElementById("animal-type-container");
+}
+
+async function predict(inputImage) {
+  // inputImage = new Image();
+  // inputImage.src = "https://newsimg.sedaily.com/2019/09/24/1VODDC54BJ_1.jpg"
+  // animalTypeContainer.appendChild(inputImage);
+  // inputImage = document.getElementById("inputImage");
+
+  const prediction = await model.predict(inputImage);
+  let maxProbability = 0;
+  let bestPrediction = undefined;
+  for (let i = 0; i < maxPredictions; i++) {
+      if(prediction[i].probability > maxProbability) {
+        maxProbability = prediction[i].probability;
+        bestPrediction = prediction[i].className;
+      }
+  }
+  
+  return bestPrediction;
+}
+
+async function predictImage(inputImage) {
+  await init();
+  const prediction = await predict(inputImage);
+  return prediction;
+}
